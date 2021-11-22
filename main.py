9205598c12f057a806bae0cbf46ff201a26400a0
IlyaG96 <ilyag_96@icloud.com>
2021-11-23 08:02:55 +1100

    count average salary HH

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import requests
 from pprint import pprint
-languages = ["python"]
+
+languages = ["python", "java", "C"]
 
 headers = {
     "User-Agent": "api-test-agent"
 }
 
-#{
+
+# {
 # vacancies_found: 6695
 # vacancies_processed 13
 # average_salary: 9988}
 
+
 def count_vacancies_found(vacancies):
-    pass
+    return vacancies["found"]
 
 
-def count_average_salary(salary):
-    pass
-
+def count_average_salary(vacancies):
+    salaries = []
+    for vacancy in vacancies["items"]:
+        salary = predict_rub_salary(vacancy)
+        if salary:
+            salaries.append(int(salary))
+    return int(sum(salaries) / len(salaries)), len(salaries)
 
 
 def predict_rub_salary(vacancy):
@@ -29,33 +36,34 @@ def predict_rub_salary(vacancy):
         elif not max_salary:
             return min_salary
         else:
-            return (min_salary+max_salary)/2
+            return (min_salary + max_salary) / 2
     return None
 
 
-def get_response_hh():
-    for language in languages:
-        payload = {
-            "text": f"{language}",
-            "area": "1",
-        }
-        url = f"https://api.hh.ru/vacancies/"
-        response = requests.get(url, headers=headers, params=payload)
-        response.raise_for_status()
-        vacancies = response.json()
-      #  pprint(vacancies) #["items"]
-        return vacancies
-   # for vacancy in vacancies:
-    #    print(vacancy)
-    #    print(predict_rub_salary(vacancy))
+def get_vacancies_hh(language):
+    payload = {
+        "text": f"{language}",
+        "area": "1",
+    }
+    url = f"https://api.hh.ru/vacancies/"
+    response = requests.get(url, headers=headers, params=payload)
+    response.raise_for_status()
+    vacancies = response.json()
+    return vacancies
+
 
 def main():
-    vacancies = get_response_hh()
-    for vacancy in vacancies:
-        salaries = [salary for salary in predict_rub_salary(vacancy) if not None]
-        print(salaries)
-        print("222")
+    vacancies_hh = {}
+    for language in languages:
+        vacancies = get_vacancies_hh(language)
+        average_salary, vacancies_processed = count_average_salary(vacancies)
+        vacancies_found = count_vacancies_found(vacancies)
+        vacancies_hh.update({
+            language: {
+                "vacancies_found": vacancies_found,
+                "vacancies_processed": vacancies_processed,
+                "average_salary": average_salary, }
+        })
 
-
-
+    print(vacancies_hh)
 main()
