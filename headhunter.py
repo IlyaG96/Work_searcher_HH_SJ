78914d6148caa07ff02ff0253a0c52b65bfab02a
IlyaG96 <ilyag_96@icloud.com>
2021-12-03 18:47:42 +1100

    repaired search logic in headhunter.py

--- headhunter.py
+++ headhunter.py
@@ -1,9 +1,7 @@
 import requests
 from salaries import predict_rub_salary_hh
-
-
-def count_vacancies_found(vacancies):
-    return vacancies["found"]
+from pprint import pprint
+from itertools import count
 
 
 def count_average_salary_and_processed_vacancies(vacancies):
@@ -13,9 +11,6 @@ def count_average_salary_and_processed_vacancies(vacancies):
         if salary:
             salaries.append(salary)
     return int(sum(salaries) / len(salaries)), len(salaries)
-
-
-
 
 
 def get_vacancies_hh(language, page):
@@ -32,20 +27,20 @@ def get_vacancies_hh(language, page):
     response = requests.get(url, headers=headers, params=payload)
     response.raise_for_status()
     vacancies = response.json()
+    pprint(vacancies)
     return vacancies
 
 
 def generate_vacancies_data_hh(languages):
     vacancies_hh = {}
-    total_pages = 1
-    pages = range(total_pages)
 
     for language in languages:
         total_average_salary = 0
         total_vacancies_processed = 0
-        for page in pages:
+        for page in count():
             vacancies = get_vacancies_hh(language, page)
-            vacancies_found = count_vacancies_found(vacancies)
+            vacancies_found = vacancies["found"]
+            total_pages = vacancies["pages"]
             average_salary, vacancies_processed = count_average_salary_and_processed_vacancies(vacancies)
 
             total_average_salary += average_salary
@@ -57,4 +52,6 @@ def generate_vacancies_data_hh(languages):
                     "vacancies_processed": total_vacancies_processed,
                     "average_salary": int(total_average_salary/total_pages)}
             })
+            if page >= total_pages-1:
+                break
     return vacancies_hh
